ac6167b9b428cb6aaa99f8ea77f64fb08a2b24e7
kizzylion <kztchm@gmail.com>
2024-09-03 18:44:32 -0400

    FEAT: ADDS RESUME LIBRARY UI

--- src/App.jsx
+++ src/App.jsx
@@ -36,10 +36,66 @@ export default function App() {
       <div className="bg-white" style={{ paddingTop: `${headerHeight}px` }}>
         <FilterSection />
       </div>
-
-      <main style={{ paddingTop: `${headerHeight}px` }}>
-        <MultiForm />
+      <main id="library" className="bg-white">
+        <div className="max-w-7xl mx-auto w-full flex flex-col px-4 md:px-6 lg:px-8 py-5 ">
+          <div className="hidden md:flex justify-between mb-1 gap-4 w-full h-fit font-semibold uppercase text-base text-900 pl-8 py-3">
+            <div className="flex w-full gap-4 ">
+              <p className="w-1/2">NAME</p>
+              <p className="w-1/2">CREATION</p>
+            </div>
+            <p className="w-1/3">ACTION</p>
+          </div>
+          <div className="resume-list flex flex-col gap-0 w-full py-3 pl-4 md:pl-8">
+            <article className="resume-item flex w-full  text-base gap-4 py-4 ">
+              <div className="w-full flex flex-col text-gray-900 md:flex-row gap-0.5 md:gap-4">
+                <p className="w-1/2 font-medium ">John_Doe_Resume_1</p>
+                <p className="w-1/2 text-gray-600 font-medium text-xs md:text-sm">
+                  <span className="md:hidden">Created:</span> 12/05/2022
+                </p>
+              </div>
+              <p className="w-1/3 text-indigo-700">
+                <i className="bi bi-pencil-square"></i>
+              </p>
+            </article>
+            <article className="resume-item flex w-full  text-base gap-4 py-4 ">
+              <div className="w-full flex flex-col text-gray-900 md:flex-row gap-0.5 md:gap-4">
+                <p className="w-1/2 font-medium ">John_Doe_Resume_1</p>
+                <p className="w-1/2 text-gray-600 font-medium text-xs md:text-sm">
+                  <span className="md:hidden">Created:</span> 12/05/2022
+                </p>
+              </div>
+              <p className="w-1/3 text-indigo-700">
+                <i className="bi bi-pencil-square"></i>
+              </p>
+            </article>
+          </div>
+        </div>
       </main>
+
+      <div style={{ paddingTop: `${headerHeight}px` }}>
+        <MultiForm />
+      </div>
+      <footer>
+        <div className="max-w-7xl mx-auto w-full flex flex-col md:flex-row md:justify-between px-4 md:px-6 lg:px-8 py-5 gap-3">
+          <div className="socials flex text-2xl text-gray-900 gap-6 w-fit">
+            <a href="mailto:kztchm@gmail.com">
+              <i className="bi bi-envelope-fill"></i>
+            </a>
+            <a href="#">
+              <i className="bi bi-facebook"></i>
+            </a>
+            <a href="#">
+              <i className="bi bi-twitter-x"></i>
+            </a>
+            <a href="#">
+              <i className="bi bi-linkedin"></i>
+            </a>
+          </div>
+          <p className="text-gray-600 font-normal text-sm">
+            Copyright &copy;2024. All rights reserved.
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
